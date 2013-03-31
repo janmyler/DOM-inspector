19257c418c00fa76dbd4c1a1105207682dd6788c
Jan Myler <info@janmyler.com>
2013-03-31 22:28:23 +0200

    Basic file structure created. Configured for SASS (Compass).

---
+++ css/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+require "compass-normalize"
+
+# Environment settings
+# environment = :production
+environment = :development
+
+# Configuration
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "../img"
+javascripts_dir = "../js"
+
+# Other settings
+output_style = (environment == :production) ? :compressed : :expanded
+relative_assets = true
+line_comments = false
